9bdf53d0f9d8707119b86c7e92c00e337fc12476
Den <den.sanches@mail.ru>
2020-02-21 15:42:59 +0300

    template

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../Header/Header.scss';
+
+class Header extends React.Component{
+   render(){
+       return(
+           <div>
+                <h2> Hello! It is React-App!</h2>
+           </div>
+       )
+   }
+}
+
+export default Header;
